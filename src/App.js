b0dd1860a547cfaf46c6f6687ff969ca5238d4c0
Björn Jansson <bjornjansson@Bjorns-iMac.local>
2019-06-12 13:58:46 +0200

    React hooks & context

--- src/App.js
+++ src/App.js
@@ -1,78 +1,42 @@
-// import React, { Component } from "react";
-// import Navbar from "./components/layout/Navbar";
-// import Users from "./components/users/Users";
-// // import Search from "./components/users/Search";
-// import axios from "axios";
-// import "./App.css";
-
-// class App extends Component {
-//   state = {
-//     users: [],
-//     loading: false
-//   };
-
-//   // Search Github users
-//   searchUsers = async text => {
-//     this.setState({ loading: true });
-
-// const res = await axios.get(
-//   `https://api.github.com/search/users?q=${text}&client_id=${
-//     process.env.REACT_APP_GITHUB_CLIENT_ID
-//   }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-// );
-
-//     this.setState({ users: res.data.items, loading: false });
-//   };
-
-//   // Clear users from state
-//   clearUsers = () => this.setState({ users: [], loading: false });
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar />
-//         <div className="container">
-//           {/* <Search
-//             searchUsers={this.searchUsers}
-//             clearUsers={this.clearUsers}
-//             showClear={this.state.users.length > 0 ? true : false}
-//           /> */}
-//           <Users loading={this.state.loading} users={this.state.users} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import axios from "axios";
-import "./App.css";
-import Users from "./components/users/Users";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
   state = {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+
+  // Search Github users
+  searchUsers = async text => {
     this.setState({ loading: true });
+
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Navbar />
-        <div className="container">
+        <div className='container'>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className="navbar bg-primary">
+    <nav className='navbar bg-primary'>
       <h1>
         <i className={icon} /> {title}
       </h1>
     </nav>
   );
 };
+
 Navbar.defaultProps = {
-  title: "Github Finder",
-  icon: "fab fa-github"
+  title: 'Github Finder',
+  icon: 'fab fa-github'
 };
 
 Navbar.propTypes = {
